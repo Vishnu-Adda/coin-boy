cefac1c5a875cb2eb417a1ab4507dc9ef2e05a21
Vishnu Adda <addavishnu@gmail.com>
2018-07-10 23:40:19 -0700

    Implement drawing bombs and coins, with basic interaction for the boy.

--- core/src/com/vadda/coinboy/game/CoinBoy.java
+++ core/src/com/vadda/coinboy/game/CoinBoy.java
@@ -5,6 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class CoinBoy extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -16,6 +21,21 @@ public class CoinBoy extends ApplicationAdapter {
 	float gravity = 0.2f;
 	float velocity = 0;
 	int manY = 0; // His location
+    Rectangle manRectangle;
+
+    Random random;
+
+	ArrayList<Integer> coinXs = new ArrayList<Integer>();
+	ArrayList<Integer> coinYs = new ArrayList<Integer>();
+	ArrayList<Rectangle> coinRectangles = new ArrayList<Rectangle>();
+	Texture coin;
+	int coinCount;
+
+    ArrayList<Integer> bombXs = new ArrayList<Integer>();
+    ArrayList<Integer> bombYs = new ArrayList<Integer>();
+    ArrayList<Rectangle> bombRectangles = new ArrayList<Rectangle>();
+    Texture bomb;
+    int bombCount;
 	
 	@Override
 	public void create () {
@@ -28,15 +48,63 @@ public class CoinBoy extends ApplicationAdapter {
 		man[3] = new Texture("frame-4.png");
 
 		manY = Gdx.graphics.getHeight() / 2;
+
+		coin = new Texture("coin.png");
+		bomb = new Texture("bomb.png");
+		random = new Random();
 	}
 
-	/**
+	public void makeCoin() {
+	    float height = random.nextFloat() * Gdx.graphics.getHeight();
+	    coinYs.add((int)height);
+	    coinXs.add(Gdx.graphics.getWidth());
+    }
+
+    public void makeBomb() {
+        float height = random.nextFloat() * Gdx.graphics.getHeight();
+        bombYs.add((int)height);
+        bombXs.add(Gdx.graphics.getWidth());
+    }
+
+    /**
 	 * This will run over and over until the end
 	 */
 	@Override
 	public void render () {
         batch.begin();
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        // BOMB DRAW
+        if(bombCount < 250) {
+            bombCount ++;
+        } else {
+            bombCount = 0;
+            makeBomb();
+        }
+
+        bombRectangles.clear();
+        for(int i = 0; i < bombXs.size(); i++) {
+            batch.draw(bomb, bombXs.get(i), bombYs.get(i));
+            bombXs.set(i, bombXs.get(i) - 6);
+            bombRectangles.add(new Rectangle(bombXs.get(i), bombYs.get(i),
+                    bomb.getWidth(), bomb.getHeight()));
+        }
+
+        // COIN DRAW
+        if(coinCount < 100) {
+            coinCount ++;
+        } else {
+            coinCount = 0;
+            makeCoin();
+        }
+
+        coinRectangles.clear();
+        for(int i = 0; i < coinXs.size(); i++) {
+            batch.draw(coin, coinXs.get(i), coinYs.get(i));
+            coinXs.set(i, coinXs.get(i) - 4);
+            coinRectangles.add(new Rectangle(coinXs.get(i), coinYs.get(i),
+                    coin.getWidth(), coin.getHeight()));
+        }
 
         // Recognizes screen touch
         if(Gdx.input.justTouched()) {
@@ -63,6 +131,23 @@ public class CoinBoy extends ApplicationAdapter {
 		}
 
         batch.draw(man[manState], Gdx.graphics.getWidth() / 2 - man[manState].getWidth() / 2, manY);
+        manRectangle = new Rectangle(Gdx.graphics.getWidth() / 2 - man[manState].getWidth() / 2,
+                manY,
+                man[manState].getWidth(),
+                man[manState].getHeight());
+
+        for (int i = 0; i < coinRectangles.size(); i++) {
+            if(Intersector.overlaps(manRectangle, coinRectangles.get(i))) {
+                Gdx.app.log("Coin", "Touched");
+            }
+        }
+
+        for (int i = 0; i < bombRectangles.size(); i++) {
+            if(Intersector.overlaps(manRectangle, bombRectangles.get(i))) {
+                Gdx.app.log("Bomb", "Touched");
+            }
+        }
+
 
         batch.end();
 	}
